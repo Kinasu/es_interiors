c0098c434beee33fbdba80c21320738e779e6afa
Kinasu <ashtoreet@gmail.com>
2016-12-28 20:03:24 +0300

    initial commit

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,16 @@
+class ImagesController < ApplicationController
+
+  def show
+    @hello = "Hello World!"
+    @lorem = " Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n"+
+            " Donec vel risus porttitor, mollis magna consequat, dignissim nisl.\n"+
+            " Nunc porta dictum massa at sollicitudin.\n"+
+            " Sed blandit pellentesque convallis.\n"+
+            " Donec tempor ante non ipsum fermentum pharetra.\n"+
+            " Integer eget semper magna, sed suscipit ipsum.\n"+
+            " Suspendisse ut gravida mi.\n"+
+            " Aliquam nisl erat, placerat at luctus laoreet, fermentum sed mauris.\n"+
+            " Integer sed placerat augue. Nunc eget elementum urna.\n"+
+            " Donec ante nunc, molestie eget nibh a, posuere auctor mauris.\n"
+  end
+end
